074cb0f52fca8e399c615b7e7dd739273cf902fa
agroribeiro-rr-br <agroribeiro@gmail.com>
2026-08-28 10:51:25 -0400

    Update swv2.js

--- sw.js
+++ sw.js
@@ -1,5 +1,55 @@
-const CACHE='usa-trip-2026-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='usa-trip-2026-v2';
+
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    Promise.all([
+      caches.keys().then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      ),
+      self.clients.claim()
+    ])
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+
+        caches.open(CACHE)
+          .then(cache => cache.put(e.request, clone));
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(e.request)
+          .then(response =>
+            response || caches.match('./index.html')
+          )
+      )
+  );
+});
